ff7229d3e85c5f64ab88bd80b5e62b805ed87254
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-11-18 14:00:51 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
+            const sid = `*✅ Season ID Received!\n🎉 Connection established successfully.\nEnjoy using Queen Asha MD Bot! 💖*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp Channel*\n\n*https://whatsapp.com/channel/0029Vb6FwIK89inhtCZOlp12/150*`;
+            const mg = `> Please keep it safe and DO NOT share it with anyone. 🔐`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://files.catbox.moe/z3v2kz.jpg",
               },
               caption: sid,
             });
@@ -129,3 +129,4 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
