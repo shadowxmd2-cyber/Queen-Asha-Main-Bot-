468215a9c47a241e744f7e93787168f8df9f2809
shadowxmd2-cyber <shadowxmd2@gmail.com>
2025-12-03 21:01:53 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -10,7 +10,7 @@ const {
   makeCacheableSignalKeyStore,
   Browsers,
   jidNormalizedUser,
-} = require("@neoxr/wb");
+} = require("@fuxxy-star/baileys");
 const { upload } = require("./mega");
 
 function removeFile(FilePath) {
@@ -129,6 +129,7 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
 
 
 
